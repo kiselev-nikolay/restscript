4442488c9f9f481a2a8ee64aa1987ef34523b9a8
Nikolay <ceo@machineand.me>
2021-04-02 17:10:30 +0300

    Syntax error

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,26 @@ import { Parser, Grammar } from "nearley"
 import grammar from "./grammar.ne"
 
 
-const data = new URLSearchParams(window.location.search)
-const code: string = data.get("code")
+// const data = new URLSearchParams(window.location.search);
+// const code: string = data.get("code");
+const gram = Grammar.fromCompiled(grammar);
 
-const parser = new Parser(Grammar.fromCompiled(grammar))
-let result = JSON.stringify(parser.feed(code).results[0])
+let el = window.document.getElementById("code");
+let input = el as HTMLInputElement;
 
-document.getElementById("result").textContent = result
+input.oninput = () => {
+    const code = input.value;
+    let result: string = null;
+    try {
+        let parser = new Parser(gram);
+        result = JSON.stringify(parser.feed(code).results[0]);
+    } catch {
+        // Syntax error.
+    }
+    if (result === null || result === undefined) {
+        result = "Syntax error."
+    }
+    document.getElementById("result").textContent = result;
+}
+
+input.focus()
